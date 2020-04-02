c3456c7eb01e3ebb9053c9cd1a2a3244322722df
Jake Gonchar <jake098877@gmail.com>
2020-04-02 17:08:37 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
+function happyHolidays() {
+  return "Happy holidays!";
+}
+happyHolidays(name)
+function happyHolidaysTo(name) {
+  return `Happy holidays, ${name}!`;
+}
+
+function happyCustomHolidayTo(holiday, name) {
+  return `Happy ${holiday}, ${name}`;
+}
+
+function holidayCountdown(days, holiday) {
+  return `It's ${days} until ${holiday}!`;
+}
 // write your code below!
